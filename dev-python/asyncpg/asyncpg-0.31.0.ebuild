parona-source/parona-overlay

commit 7a473899c7f490a22818c045c791160c19bd3f8e

dev-python/asyncpg: enable py3.15
Signed-off-by: Alfred Wingate <parona@protonmail.com>

dev-python/asyncpg/asyncpg-0.31.0.ebuild
@@ -5,7 +5,7 @@ EAPI=8
 
 DISTUTILS_EXT=1
 DISTUTILS_USE_PEP517=setuptools
-PYTHON_COMPAT=( python3_{12..14} )
+PYTHON_COMPAT=( python3_{12..15} )
 
 inherit distutils-r1
 
@@ -39,7 +39,9 @@ BDEPEND="
 		dev-python/distro[${PYTHON_USEDEP}]
 		dev-python/gssapi[${PYTHON_USEDEP}]
 		dev-python/k5test[${PYTHON_USEDEP}]
-		dev-python/uvloop[${PYTHON_USEDEP}]
+		$(python_gen_cond_dep '
+			dev-python/uvloop[${PYTHON_USEDEP}]
+		' python3_{12..14})
 	)
 "
 

commit 8236012b1e74b8280495e9027763cbb8b3a93311

dev-python/asyncpg: add todo comment
Fixes: 7a473899c7f490a22818c045c791160c19bd3f8e
Signed-off-by: Alfred Wingate <parona@protonmail.com>

dev-python/asyncpg/asyncpg-0.31.0.ebuild
@@ -30,6 +30,8 @@ KEYWORDS="~amd64"
 RDEPEND="
 	dev-db/postgresql
 "
+
+# TODO: python3.15 uvloop
 # pkg-resources: https://github.com/MagicStack/asyncpg/pull/1314
 BDEPEND="
 	<dev-python/cython-4.0.0[${PYTHON_USEDEP}]