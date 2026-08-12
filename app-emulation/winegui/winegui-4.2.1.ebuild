parona-source/parona-overlay

commit 7954e11443716c9ecbeb507661bb4d13ae9af9d3

app-emulation/winegui: wine needed for tests
Signed-off-by: Alfred Wingate <parona@protonmail.com>

app-emulation/winegui/winegui-4.2.1.ebuild
@@ -36,6 +36,7 @@ DEPEND="${RDEPEND}
 BDEPEND="
 	virtual/pkgconfig
 	doc? ( app-text/doxygen[dot] )
+	test?( virtual/wine )
 "
 
 PATCHES=(

commit 02e15de44d0416fe68bcca0f9bae8397f4b2f71d

app-emulation/winegui: fix invalid BDEPEND
Thought pkgcheck and pkgcruft would catch this loudly.

Fixes: 7954e11443716c9ecbeb507661bb4d13ae9af9d3
Signed-off-by: Alfred Wingate <parona@protonmail.com>

app-emulation/winegui/winegui-4.2.1.ebuild
@@ -36,7 +36,7 @@ DEPEND="${RDEPEND}
 BDEPEND="
 	virtual/pkgconfig
 	doc? ( app-text/doxygen[dot] )
-	test?( virtual/wine )
+	test? ( virtual/wine )
 "
 
 PATCHES=(